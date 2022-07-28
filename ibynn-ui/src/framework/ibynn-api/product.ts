e3887fc1f55cb016ce1a13c3a715c1e9ef5a5750
Marc Rivas <76574248+mrevas@users.noreply.github.com>
2022-07-28 19:51:14 -0400

    Update product.ts

--- ibynn-ui/src/framework/ibynn-api/product.ts
+++ ibynn-ui/src/framework/ibynn-api/product.ts
@@ -6,7 +6,7 @@ export const searchForProduct = async (query: string|string[]|undefined) => {
     try {
         
         const { data, status } = await axios.get<Products>(
-            `https://oyster-app-xxbzd.ondigitalocean.app:9476/searchAllStores?q=${query}`,
+            `https:/api.ibynn.com/searchAllStores?q=${query}`,
             {
                 headers: {
                 Accept: 'application/json',
