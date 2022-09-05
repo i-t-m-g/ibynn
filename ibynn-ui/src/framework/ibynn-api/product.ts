3477eb64c6fcf19c53a6c8368e73f0c13ca2426e
gaminov1 <gaminov1@users.noreply.github.com>
2022-09-05 18:19:01 -0400

    Added detailed queries for most categories

--- ibynn-ui/src/framework/ibynn-api/product.ts
+++ ibynn-ui/src/framework/ibynn-api/product.ts
@@ -1,41 +1,33 @@
-import axios from "axios";
-import { ParsedUrlQuery } from "querystring";
-import { Products } from "./entities/product";
+import axios from 'axios';
+import { ParsedUrlQuery } from 'querystring';
+import { Products } from './entities/product';
 
 export const searchForProduct = async (query: ParsedUrlQuery) => {
-    try {
-        
-        const { data, status } = await axios.get<Products>(
-            `${process.env.NEXT_PUBLIC_REST_API_ENDPOINT}/shopping?q=${query.q}&sortBy=${query.sortBy}`,
-            {
-                headers: {
-                    Accept: 'application/json',
-                },
-            },
-        );
+  try {
+    const { data, status } = await axios.get<Products>(
+      `${process.env.NEXT_PUBLIC_REST_API_ENDPOINT}/shopping?q=${query.q}&sortBy=${query.sortBy}`,
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      }
+    );
 
-        const sortedData = data.shopping_results.sort((a, b) => {
-            console.log(a.unit_price_displayed)
-            if (a.extracted_price && b.extracted_price) {
-                return a.extracted_price - b.extracted_price;
-            }
+    const sortedData = data.shopping_results.sort((a, b) => {
+      if (a.extracted_price && b.extracted_price) {
+        return a.extracted_price - b.extracted_price;
+      }
+      return 0;
+    });
 
-            return 0;
-            
-            }
-        );
-
-        data.shopping_results = sortedData;
-        return data;
-
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            console.log('error message: ', error.message);
-            
-        } else {
-            console.log('unexpected error: ', error);
-            
-        }
-        return new Products()
+    data.shopping_results = sortedData;
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+    } else {
+      console.log('unexpected error: ', error);
     }
-}
+    return new Products();
+  }
+};
