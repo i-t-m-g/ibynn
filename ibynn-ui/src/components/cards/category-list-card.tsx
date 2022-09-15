171295af73d750172f25c7d302f6f2fe69113118
gaminov1 <gaminov1@users.noreply.github.com>
2022-09-15 18:40:52 -0400

    images for food cats

--- ibynn-ui/src/components/cards/category-list-card.tsx
+++ ibynn-ui/src/components/cards/category-list-card.tsx
@@ -77,8 +77,8 @@ const CategoryListCard: React.FC<Props> = ({
         <Image
           src={icon ?? '/assets/placeholder/category-small.svg'}
           alt={name || t('text-category-thumbnail')}
-          width={100}
-          height={100}
+          width={120}
+          height={120}
         />
         {/* </div> */}
         {/* {category.children && category.children.length > 0 && <div className="flex items-center transition-all transform group-hover:translate-x-1"> */}
