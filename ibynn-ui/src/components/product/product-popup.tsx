a8b2fcd9b5881a2d9145ba8b3699bb5bef5e58c1
gaminov1 <gaminov1@users.noreply.github.com>
2022-10-27 17:38:44 -0400

    Added modal updates

--- ibynn-ui/src/components/product/product-popup.tsx
+++ ibynn-ui/src/components/product/product-popup.tsx
@@ -1,66 +1,26 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import isEmpty from 'lodash/isEmpty';
 import { ROUTES } from '@utils/routes';
-import Button from '@components/ui/button';
-import Counter from '@components/ui/counter';
-import { useCart } from '@contexts/cart/cart.context';
-import ProductAttributes from '@components/product/product-attributes';
-import { generateCartItem } from '@utils/generate-cart-item';
-import usePrice from '@framework/product/use-price';
-import { getVariations } from '@framework/utils/get-variations';
 import { useTranslation } from 'next-i18next';
-import ThumbnailCarousel from '@components/ui/carousel/thumbnail-carousel';
-import Image from '@components/ui/image';
-import CartIcon from '@components/icons/cart-icon';
-import Heading from '@components/ui/heading';
-import Text from '@components/ui/text';
-import TagLabel from '@components/ui/tag-label';
-import LabelIcon from '@components/icons/label-icon';
-import { IoArrowRedoOutline } from 'react-icons/io5';
-import RelatedProductFeed from '@components/product/feeds/related-product-feed';
-import SocialShareBox from '@components/ui/social-share-box';
-import { IoIosHeart, IoIosHeartEmpty } from 'react-icons/io';
-import { toast } from 'react-toastify';
 import useWindowSize from '@utils/use-window-size';
 import {
   useModalAction,
   useModalState,
 } from '@components/common/modal/modal.context';
 import CloseButton from '@components/ui/close-button';
-import VariationPrice from './variation-price';
-import isEqual from 'lodash/isEqual';
-import { productGalleryPlaceholder } from '@assets/placeholders';
-
-const breakpoints = {
-  '1536': {
-    slidesPerView: 6,
-  },
-  '1280': {
-    slidesPerView: 5,
-  },
-  '1024': {
-    slidesPerView: 4,
-  },
-  '640': {
-    slidesPerView: 3,
-  },
-  '360': {
-    slidesPerView: 2,
-  },
-  '0': {
-    slidesPerView: 1,
-  },
-};
+import ModalProductDetails from './modal-product-details';
+import axios from 'axios';
 
 export default function ProductPopup() {
-  const { t } = useTranslation('common');
   const { data } = useModalState();
-  const { width } = useWindowSize();
   const { closeModal } = useModalAction();
-  const router = useRouter();   
-  const { slug, image, name, gallery} = data;
+  const { slug, title } = data;
   const productUrl = `${process.env.NEXT_PUBLIC_WEBSITE_URL}${ROUTES.PRODUCT}/${slug}`;
+  const url = `${process.env.NEXT_PUBLIC_REST_API_ENDPOINT}/compare?q=${title}}`;
+
+  useEffect(() => {
+    const response = axios.get(url).then((res) => console.log(res.data));
+  }, []);
 
   return (
     <div className="md:w-[600px] lg:w-[940px] xl:w-[1180px] 2xl:w-[1360px] mx-auto p-1 lg:p-0 xl:p-3 bg-brand-light rounded-md">
@@ -72,12 +32,13 @@ export default function ProductPopup() {
               {
                 <div className="flex items-center justify-center w-auto">
                   <img
-                    style={{width: "250px"}}
+                    style={{ width: '250px' }}
                     src={data.thumbnail}
-                    alt={name!}
+                    alt={title}
                   />
                 </div>
               }
+              <ModalProductDetails />
             </div>
           </div>
         </div>
