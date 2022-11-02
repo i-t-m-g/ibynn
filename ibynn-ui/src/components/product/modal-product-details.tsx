86b81872e7524f4c6efd9a491be7027a4cbcdbb4
gaminov1 <gaminov1@users.noreply.github.com>
2022-11-02 15:54:20 -0400

    Update modal-product-details.tsx

--- ibynn-ui/src/components/product/modal-product-details.tsx
+++ ibynn-ui/src/components/product/modal-product-details.tsx
@@ -7,30 +7,29 @@ import Heading from '@components/ui/heading';
 
 const OrderItemCard: any = ({ product }) => {
   return (
-    <tr
-      className="font-normal border-b border-border-base last:border-b-0"
-    >
-      <td className="p-4" style={{color: 'blue'}}>
-        <a href={product?.link} target="_blank" rel="noreferrer">{product?.title}</a>
+    <tr className="font-normal border-b border-border-base last:border-b-0">
+      <td className="p-4" style={{ color: 'blue' }}>
+        <a href={product?.link} target="_blank" rel="noreferrer">
+          {product?.title}
+        </a>
       </td>
-      <td className="p-4">
-        ${product?.extracted_price}
-      </td>
-      <td className="p-4">
-        {product?.source}
-      </td>
+      <td className="p-4">${product?.extracted_price}</td>
+      <td className="p-4">{product?.source}</td>
     </tr>
   );
 };
 
-const ModalProductDetails: React.FC<any> = ({className = 'pt-10 lg:pt-12', product}) => {
-  console.log("this is it: -----",product);
+const ModalProductDetails: React.FC<any> = ({
+  className = 'pt-10 lg:pt-12',
+  product,
+}) => {
+  console.log('this is it: -----', product);
   const { t } = useTranslation('common');
 
   return (
     <div className={className}>
       <Heading variant="heading" className="mb-6 xl:mb-7">
-        {t('text-order-details')}:
+        Comparing Price:
       </Heading>
       <table className="w-full text-sm font-semibold text-brand-dark lg:text-base">
         <thead>
@@ -47,9 +46,13 @@ const ModalProductDetails: React.FC<any> = ({className = 'pt-10 lg:pt-12', produ
           </tr>
         </thead>
         <tbody>
-          {product?.length > 0 ? product.map((product: any, index: number) => (
-            <OrderItemCard key={index} product={product} />
-          )): <p>Loading...</p>}
+          {product?.length > 0 ? (
+            product.map((product: any, index: number) => (
+              <OrderItemCard key={index} product={product} />
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
         </tbody>
       </table>
     </div>
