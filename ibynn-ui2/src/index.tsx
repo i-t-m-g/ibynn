be5781fadd8fc9a4950d1aa59b6ede43e54c848e
Gavriel Aminov <gavrealaminov@gmail.com>
2022-12-25 11:38:36 -0500

    Update index.tsx

--- ibynn-ui2/src/index.tsx
+++ ibynn-ui2/src/index.tsx
@@ -18,12 +18,12 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
-  // </React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
