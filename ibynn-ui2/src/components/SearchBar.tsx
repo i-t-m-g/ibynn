3a9408b235b658bdc0b5b0cb0716df1735e42319
Gavriel Aminov <gavrealaminov@gmail.com>
2023-01-04 13:37:41 -0500

    Update SearchBar.tsx

--- ibynn-ui2/src/components/SearchBar.tsx
+++ ibynn-ui2/src/components/SearchBar.tsx
@@ -15,7 +15,7 @@ export default function SearchBar({array, setActiveResults}:any) {
     };
 
   return (
-    <div className='fixed w-full z-50'>   
+    <div className='fixed w-full z-30'>   
         <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
