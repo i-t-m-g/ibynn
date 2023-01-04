74b08514795f034a60a47043003a4ad0e6d96f9f
Gavriel Aminov <gavrealaminov@gmail.com>
2023-01-03 19:03:56 -0500

    Remove sub headings under the category name on Category page

--- ibynn-ui2/src/containers/PageCollection.tsx
+++ ibynn-ui2/src/containers/PageCollection.tsx
@@ -37,10 +37,6 @@ const PageCollection: FC<PageCollectionProps> = ({ className = "" }) => {
             <h2 className="block text-2xl sm:text-3xl lg:text-4xl font-semibold">
               {dc.activeCategory && dc.activeCategory?.name}
             </h2>
-            <span className="block mt-4 text-neutral-500 dark:text-neutral-400 text-sm sm:text-base">
-              We not only help you design exceptional products, but also make it
-              easy for you to share your designs with more like-minded people.
-            </span>
           </div>
 
           <hr className="border-slate-200 dark:border-slate-700" />
